4c53e4c718a31e2fd6a3e1d7fdbcc09d8036d412
Brian Kozub <ticet11@gmail.com>
2020-07-19 16:29:54 -0600

    removed console logs

--- src/App.js
+++ src/App.js
@@ -73,7 +73,6 @@ const Timer = () => {
     };
 
     const incrementField = (type) => {
-        console.log(type);
         if (type === "hours") {
             setHours(parseInt(hours) + 1);
         } else if (type === "minutes") {
